1c793404f9fd3d2f0dc69eb90d57ae890411cdfe
de Sousa Barbosa Diego <81906437+DeSousaDiego@users.noreply.github.com>
2023-04-27 16:25:44 +0200

    Add files via upload

---
+++ testProject/src/Controller.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void startButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();   
+        scene = new Scene(root);  
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+        // Stage mainWindow = (Stage) tfTitle.getScene().getWindow();$
+    }
+
+}
