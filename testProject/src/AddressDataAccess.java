e08f6a037b05d6442df6c66f78c8e17703889537
de Sousa Barbosa Diego <81906437+DeSousaDiego@users.noreply.github.com>
2023-05-16 13:16:18 +0200

    Delete testProject/src directory

--- testProject/src/AddressDataAccess.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.ArrayList;
-
-public interface AddressDataAccess {
-    void createAddress(int idLocality, String street, int number);
-    ArrayList<Address> readAddresses();
-    Address readOneAddress(int id);
-    void addressIn(Locality locality, String street, int number);
-}
