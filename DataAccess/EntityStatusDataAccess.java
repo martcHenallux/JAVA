7db68d08f0ac5184a0e41b86ca42777f9fe95c20
Cédric M <128589682+martcHenallux@users.noreply.github.com>
2023-05-16 13:15:26 +0200

    Delete DataAccess directory

--- DataAccess/EntityStatusDataAccess.java
+++
@@ -1,9 +0,0 @@
-package DataAccess;
-import java.util.ArrayList;
-
-import Model.EntityStatus;
-
-public interface EntityStatusDataAccess {
-    ArrayList<EntityStatus> readEntityStatus();
-    EntityStatus readOneEntityStatus(String code);
-}
